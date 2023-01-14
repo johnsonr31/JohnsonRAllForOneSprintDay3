7af16a035aabac1617f07e690273c78c91c4dc00
ricRobo4 <artistr3@proton.me>
2023-01-13 20:54:08 -0800

    started on page 9

--- scripts/page9.js
+++ scripts/page9.js
@@ -1,3 +1,10 @@
+let fastFoodBtn = document.getElementById("fastFoodBtn");
+let chineseBtn = document.getElementById("chineseBtn");
+let italianBtn = document.getElementById("italianBtn");
+let restaurantResult = document.getElementById("restaurantResult");
+
+
+
 function getAPI() {
     fetch("https://richardallforone.azurewebsites.net/StudentDirectory/restaurantpicker/FastFood")
     .then((response) => response.text())
